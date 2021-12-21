a2cce25350c7f662d48bc610af2ccdc1407c911b
Pluto-yubin <1113940328@qq.com>
2021-12-21 23:45:57 +0800

    Test Null Argument

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList res = A;
         while (A.rest != null) {
             A = A.rest;
@@ -96,6 +99,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList res = new IntList(A.first, null);
         IntList next = A.rest, ptr = res;
         while (next != null) {
