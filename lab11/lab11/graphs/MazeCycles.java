ac4287ae6dda68307e2e7386205ac804c1fc8cdc
Pluto-yubin <1113940328@qq.com>
2022-03-10 23:18:15 +0800

    finish MazeCycles

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -10,15 +13,45 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int[] pathTo;
+    private boolean cycleFind = false;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        pathTo = new int[m.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        findCycles(0);
     }
 
     // Helper methods go here
+    public void findCycles(int start) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(start);
+        int i = 0;
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            marked[v] = true;
+            announce();
+            for (int adj : maze.adj(v)) {
+                if (!marked[adj]) {
+                    pathTo[adj] = v;
+                    stack.push(adj);
+                } else if (adj != pathTo[v]) {
+                    edgeTo[adj] = v;
+                    announce();
+                    int cur = v;
+                    while (cur != adj) {
+                        edgeTo[cur] = pathTo[cur];
+                        announce();
+                        cur = pathTo[cur];
+                    }
+                    return;
+                }
+            }
+        }
+    }
 }
 
