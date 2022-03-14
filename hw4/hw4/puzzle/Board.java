23dda348903c4471eb028935d63619d77b5763c0
Pluto-yubin <1113940328@qq.com>
2022-03-14 22:25:49 +0800

    finish local test

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,11 +1,18 @@
 package hw4.puzzle;
 import edu.princeton.cs.algs4.Queue;
 
+
 public class Board implements WorldState {
     private int[][] tiles;
     private static final int BLANK = 0;
     public Board(int[][] tiles) {
-        this.tiles = tiles;
+        int[][] temp = new int[tiles.length][tiles.length];
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                temp[i][j] = tiles[i][j];
+            }
+        }
+        this.tiles = temp;
     }
 
     public int tileAt(int i, int j) {
@@ -19,7 +26,6 @@ public class Board implements WorldState {
      * @author http://joshh.ug/neighbors.html
      * @return
      */
-    @Override
     public Iterable<WorldState> neighbors() {
         Queue<WorldState> neighbors = new Queue<>();
         int hug = size();
@@ -53,39 +59,6 @@ public class Board implements WorldState {
         }
         return neighbors;
     }
-//    public Iterable<WorldState> neighbors() {
-//        Queue<WorldState> neighbors = new Queue<>();
-//        int hug = size();
-//        int bug = -1;
-//        int zug = -1;
-//        for (int rug = 0; rug < hug; rug++) {
-//            for (int tug = 0; tug < hug; tug++) {
-//                if (tileAt(rug, tug) == BLANK) {
-//                    bug = rug;
-//                    zug = tug;
-//                }
-//            }
-//        }
-//        int[][] ili1li1 = new int[hug][hug];
-//        for (int pug = 0; pug < hug; pug++) {
-//            for (int yug = 0; yug < hug; yug++) {
-//                ili1li1[pug][yug] = tileAt(pug, yug);
-//            }
-//        }
-//        for (int l11il = 0; l11il < hug; l11il++) {
-//            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
-//                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
-//                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
-//                    ili1li1[l11il][lil1il1] = BLANK;
-//                    Board neighbor = new Board(ili1li1);
-//                    neighbors.enqueue(neighbor);
-//                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
-//                    ili1li1[bug][zug] = BLANK;
-//                }
-//            }
-//        }
-//        return neighbors;
-//    }
 
     public int size() {
         return tiles.length;
@@ -95,7 +68,10 @@ public class Board implements WorldState {
         int dis = 0;
         for (int i = 0; i < size(); i++) {
             for (int j = 0; j < size(); j++) {
-                if (tileAt(i, j) != XYTo1D(i, j)) {
+                if (tileAt(i, j) == BLANK) {
+                    continue;
+                }
+                if (tileAt(i, j) != xyTo1D(i, j)) {
                     dis += 1;
                 }
             }
@@ -103,8 +79,8 @@ public class Board implements WorldState {
         return dis;
     }
 
-    private int XYTo1D(int x, int y) {
-        return x * size() + y;
+    private int xyTo1D(int x, int y) {
+        return x * size() + y + 1;
     }
 
     private int toX(int v) {
@@ -137,8 +113,14 @@ public class Board implements WorldState {
     }
 
     public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
         if (!(y instanceof Board)) {
             return false;
+        }
+        if (y.hashCode() == hashCode()) {
+            return true;
         }
         for (int i = 0; i < size(); i++) {
             for (int j = 0; j < size(); j++) {
@@ -149,7 +131,13 @@ public class Board implements WorldState {
         }
         return true;
     }
-    /** Returns the string representation of the board. 
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    /** Returns the string representation of the board.
       * Uncomment this method. */
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -157,7 +145,7 @@ public class Board implements WorldState {
         s.append(N + "\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
+                s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
         }
