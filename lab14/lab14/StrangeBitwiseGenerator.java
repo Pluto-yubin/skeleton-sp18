46d77abb011dd6d2da0915aa6797c09c71577e7e
Pluto-yubin <1113940328@qq.com>
2022-03-21 00:51:28 +0800

    Finish StrangeBitwise

---
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -0,0 +1,26 @@
+package lab14;
+
+import lab14lib.Generator;
+
+/**
+ * @auther Zhang Yubin
+ * @date 2022/3/21 0:44
+ */
+public class StrangeBitwiseGenerator implements Generator {
+    private int state;
+    private int period;
+
+    public StrangeBitwiseGenerator(int period) {
+        this.period = period;
+    }
+
+    public double next() {
+        state = (state + 1);
+        int weirdState = state & (state >> 3) & (state >> 8) % period;
+        return stateHelper(weirdState);
+    }
+
+    private double stateHelper(int state) {
+        return (state % period) * 2 / (double) period - 1;
+    }
+}
