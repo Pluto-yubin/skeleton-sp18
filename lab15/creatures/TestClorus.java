3615c0e3d248795356662bada0bdc630367d80b5
Pluto-yubin <1113940328@qq.com>
2022-03-21 16:14:12 +0800

    add testcase

--- lab15/creatures/TestClorus.java
+++ lab15/creatures/TestClorus.java
@@ -1,8 +1,16 @@
 package creatures;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * @auther Zhang Yubin
  * @date 2022/3/21 15:09
  */
 public class TestClorus {
+    @Test
+    public void testAttack() {
+        Clorus clorus = new Clorus(2);
+        clorus.attack(new Plip(0.3));
+        assertEquals(2.3, clorus.energy(), 0.01);
+    }
 }
